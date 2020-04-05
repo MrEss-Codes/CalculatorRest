db45e140abac4f1313dcaedbdc424543a8b2fc1b
MrEss-Codes <57900884+MrEss-Codes@users.noreply.github.com>
2020-04-05 16:49:30 +0200

    Update ICalculatorService.java

--- src/main/java/Calculator/Core/Service/ICalculatorService.java
+++ src/main/java/Calculator/Core/Service/ICalculatorService.java
@@ -4,7 +4,19 @@ package Calculator.Core.Service;
 
 
 public interface ICalculatorService {
-
+    
+    //Sanitizer needs to sanitize whatever input, 
+    //as to make sure that the numbers are valid 
+    //and that the input can be calculated by the calculator
     public String[] sanitizer (String numbers);
-    public Float add (String input);
+    //Divison calculation should be seperated by comma
+    public Float division (String input);
+    //check if a number is a prime
+    public Bool PrimeVerification (String input);
+    //Perform sqaureroot of a given number, seperated by comma
+    public Float Squareroot (String input);
+    //multiplication 
+    public Float Multiply (String input);
+    //addition or subtraction
+    public Float AddSub (String input);
 }
